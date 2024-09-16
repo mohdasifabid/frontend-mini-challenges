991fa85c0051971acd29d09014d37312184bc350
Mohd Asif <masifabid@outlook.com>
2024-09-16 18:13:35 +0530

    Feat React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import './App.css'
-import { Counter } from './Components/Counter'
+import "./App.css";
+import { Counter } from "./Components/Counter";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-     <Counter />
+      <Router>
+        <Routes>
+          <Route path="/counter" element={<Counter />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
